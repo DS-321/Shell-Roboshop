8461298aa4391da1962e91bd6bb132805f7deb6d
SharmilaD <sharmiladaram419@gmail.com>
2025-05-27 14:52:12 +0530

    roboshop instance created

--- 01.roboshop.sh
+++ 01.roboshop.sh
@@ -10,11 +10,11 @@ DOMAIN_NAME="dcloudlab.site"
 for instance in $@
 
 do
-
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-063265bfaadd1ddd9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro
+    --security-group-ids sg-063265bfaadd1ddd9 --tag-specifications "ResourceType=instance,Tags=
+    {Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]
-
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
