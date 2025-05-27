2a9b09e96e78b0063b4ce1598b821a850a63d554
SharmilaD <sharmiladaram419@gmail.com>
2025-05-27 14:54:31 +0530

    roboshop instance created

--- 01.roboshop.sh
+++ 01.roboshop.sh
@@ -10,8 +10,8 @@ DOMAIN_NAME="dcloudlab.site"
 for instance in $@
 
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro
-    --security-group-ids sg-063265bfaadd1ddd9 --tag-specifications "ResourceType=instance,Tags=
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
+    --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=
     {Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]
